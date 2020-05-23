987293760b3adb1ebe740a882d08f24160c849fc
Gummudavelli sairam <65708703+Gummudavellisairam@users.noreply.github.com>
2020-05-23 13:12:21 +0530

    Add files via upload

---
+++ postgresql_installation1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+echo $(yum install -y postgresql-server postgresql-contrib)
+if [ "$?" = 0 ]
+then
+   echo " Postgresql Installed successfully"
+else
+   echo "\e[1;31m ERROR: Postgresql not installed \e[0m"
+fi
+fi
+fi
